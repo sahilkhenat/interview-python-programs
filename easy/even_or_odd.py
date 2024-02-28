66abfb817ecc5acc838760fe6d8e5fa15d3c376d
Sahil <khenatsahil@gmail.com>
2024-02-28 12:04:10 +0530

    Adds program for even or odd

---
+++ easy/even_or_odd.py
@@ -0,0 +1,20 @@
+"""Write a program to check if given number is prime or not,"""
+import math
+
+
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
+def main():
+    num = int(input("Enter a positive number to check if it is prime or composite:"))
+    if check_even_or_odd(num):
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+
+
+if __name__ == "__main__":
+    main()
