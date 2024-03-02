4b6678ece4c762ee95b576ec3599164febccdb6f
Sahil <khenatsahil@gmail.com>
2024-03-02 09:13:09 +0530

    Adds program to check if given year is leap or not

---
+++ easy/check_leap_year.py
@@ -0,0 +1,20 @@
+"""Write a program to check if given year is a leap year or not."""
+
+
+def is_leap_year(year):
+    if (year % 4 == 0 and year %100 !=0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+def main():
+    year = int(input("Enter year that you want to check if it is leap or not:"))
+    if is_leap_year(year):
+        print(f"{year} is a leap year")
+    else:
+        print(f"{year} is not a leap year")
+
+
+if __name__ == "__main__":
+    main()
