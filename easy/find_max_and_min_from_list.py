00d13e97e2a38d60748630e821608a47898289c1
Sahil <khenatsahil@gmail.com>
2024-03-03 20:55:10 +0530

    Adds program to find max and min

---
+++ easy/find_max_and_min_from_list.py
@@ -0,0 +1,20 @@
+"""
+Find Maximum and Minimum: Find the maximum and minimum element in an array.
+"""
+
+
+def find_max_and_min_from_list(numbers):
+    maximum, minimum = max(numbers), min(numbers)
+    return maximum, minimum
+
+
+def main():
+    num_input = input("Enter numbers separated by spaces: ")
+    num_list = num_input.split()
+    num_list = [int(num) for num in num_list]
+    maximum, minimum = find_max_and_min_from_list(num_list)
+    print(f"From {num_list} , maximum = {maximum} and minimum = {minimum} ")
+
+
+if __name__ == "__main__":
+    main()
