9fb3d6668fef8a06ae02fecf3dae47a89fce2346
Sahil <khenatsahil@gmail.com>
2024-06-05 17:48:23 +0530

    Adds program to find duplicate numbers in given list of numbers.

--- easy/find_duplicate_numbers_from_list.py
+++ easy/find_duplicate_numbers_from_list.py
@@ -21,7 +21,7 @@ def find_duplicates_in_list(numbers):
 def main():
     numbers = input_util.get_list_of_integers_from_input()
     duplicate_numbers = find_duplicates_in_list(numbers)
-    print(f"Duplicate numbers in {numbers} are: {duplicate_numbers}")
+    print(f"Duplicate numbers in given list {numbers} are: {duplicate_numbers}")
 
 
 if __name__ == "__main__":
